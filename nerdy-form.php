15f4bdac8f4a6534d000f8d17a539173a5106aa1
M.Mazzulli <38134803+mmazzulli@users.noreply.github.com>
2023-01-27 15:56:13 +0000

    first file
    Create a first file

---
+++ nerdy-form.php
@@ -0,0 +1,10 @@
+<?php 
+
+/**
+* Plugin Name: Nerdy Form
+* Plugin URI: https://spotsite.eu/plugins/nerdy-form/
+* Description: This is just a Challenge plugin with form
+* Author: Marcelo Mazzulli
+* Version: 1.0.0
+* Author URI: https://spotsite.eu/plugin/author/
+**/
